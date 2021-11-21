baf1fa6155a94c71d63dbd2d3d01ca68b2d27071
RuchikaSuryawanshi7 <ruchikasuryawanshi710@gmail.com>
2021-11-21 21:27:20 +0530

    dd

---
+++ JAVASCRIPT/base.js
@@ -0,0 +1,25 @@
+function getMilk(money) { 
+  bottle = money / 40;
+  console.log("Buying "+ bottle+ " of milk");
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+var oneLiterPrice = 40;
+var money = 120;
+getMilk(money);
